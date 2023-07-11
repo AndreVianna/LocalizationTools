587a4c54f610829fced76001ec3c8a83353ca32b
AndreVianna <andre.vianna.rj@hotmail.com>
2023-07-11 01:17:01 -0400

    Started unit tests

--- Source/LocalizationProvider.PostgreSql.UnitTests/PostgreSqlLocalizationProviderTests.cs
+++ Source/LocalizationProvider.PostgreSql.UnitTests/PostgreSqlLocalizationProviderTests.cs
@@ -4,10 +4,11 @@ using DateTimeFormat = LocalizationProvider.Contracts.DateTimeFormat;
 
 namespace LocalizationProvider.PostgreSql;
 
-public class PostgreSqlLocalizationProviderTests {
+public sealed class PostgreSqlLocalizationProviderTests : IDisposable {
     private readonly ServiceCollection _serviceCollection;
     private readonly LocalizationDbContext _dbContext;
     private readonly Application _application;
+    private readonly PostgreSqlLocalizationProvider _provider;
 
     public PostgreSqlLocalizationProviderTests() {
         var applicationId = Guid.NewGuid();
@@ -24,16 +25,20 @@ public class PostgreSqlLocalizationProviderTests {
             options.EnableDetailedErrors();
             options.EnableSensitiveDataLogging();
         });
-        var serviceManager = _serviceCollection.BuildServiceProvider();
-        _dbContext = serviceManager.GetRequiredService<LocalizationDbContext>();
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+        _dbContext = serviceProvider.GetRequiredService<LocalizationDbContext>();
         _dbContext.Applications.Add(_application);
         _dbContext.SaveChanges();
+
+        _provider = CreateProvider();
     }
 
-    private PostgreSqlLocalizationProvider CreateManager(Guid? applicationId = null) {
+    public void Dispose() => _provider.Dispose();
+
+    private PostgreSqlLocalizationProvider CreateProvider(Guid? applicationId = null) {
         _serviceCollection.AddSingleton<LocalizationOptions>(_ => new() { ApplicationId = applicationId ?? _application.Id });
-        var serviceManager = _serviceCollection.BuildServiceProvider();
-        return new(serviceManager);
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+        return new(serviceProvider);
     }
 
     [Fact]
@@ -42,7 +47,7 @@ public class PostgreSqlLocalizationProviderTests {
         var invalidId = Guid.NewGuid();
 
         // Act
-        Action act = () => CreateManager(invalidId);
+        Action act = () => CreateProvider(invalidId);
 
         // Assert
         act.Should().Throw<InvalidOperationException>()
@@ -50,12 +55,9 @@ public class PostgreSqlLocalizationProviderTests {
     }
 
     [Fact]
-    public void ForReadOnly_ThrowsException_WhenCultureNotAvailable() {
-        // Arrange
-        var manager = CreateManager();
-
+    public void SetCulture_ThrowsException_WhenCultureNotAvailable() {
         // Act
-        Action act = () => manager.ForReadOnly("es-MX");
+        Action act = () => _provider.ForReadOnly("es-MX");
 
         // Assert
         act.Should().Throw<InvalidOperationException>()
@@ -63,35 +65,35 @@ public class PostgreSqlLocalizationProviderTests {
     }
 
     [Fact]
-    public void ForUpdate_ThrowsException_WhenCultureNotAvailable() {
-        // Arrange
-        var manager = CreateManager();
-
+    public void ForReadOnly_ReturnsProvider() {
         // Act
-        Action act = () => manager.ForUpdate("es-MX");
+        var result = _provider.ForReadOnly("en-CA");
 
         // Assert
-        act.Should().Throw<InvalidOperationException>()
-           .WithMessage($"Culture 'es-MX' is not available for application '{_application.Name}'.");
+        result.Should().BeSameAs(_provider);
     }
 
     [Fact]
-    public void FindText_DateTimeManagerFormat_ReturnsCorrectFormat_WhenResourceExists() {
+    public void ForUpdate_ReturnsProvider() {
+        // Act
+        var result = _provider.ForUpdate("en-CA");
+
+        // Assert
+        result.Should().BeSameAs(_provider);
+    }
+
+    [Fact]
+    public void Dispose_DoNotThrowWhenCalledTwice()
+        => _provider.Dispose();
+
+    [Fact]
+    public void FindText_DateTimeProviderFormat_ReturnsCorrectFormat_WhenResourceExists() {
         // Arrange
         var key = Keys.GetDateTimeFormatKey(DateTimeFormat.LongDateTimePattern);
-        _dbContext.Texts
-            .Add(new() {
-                Id = 1234,
-                Key = key,
-                ApplicationId = _application.Id,
-                Culture = "en-US",
-                Value = "MMMM dd, yyyy",
-            });
-        _dbContext.SaveChanges();
-        var manager = CreateManager();
+        SeedText(key, "MMMM dd, yyyy");
 
         // Act
-        var result = manager.FindText(key);
+        var result = _provider.FindText(key);
 
         // Assert
         var subject = result.Should().BeOfType<LocalizedText>().Subject;
@@ -103,19 +105,10 @@ public class PostgreSqlLocalizationProviderTests {
     public void FindText_ForNumberFormat_ReturnsCorrectFormat_WhenResourceExists() {
         // Arrange
         var key = Keys.GetNumberFormatKey(NumberFormat.CurrencyPattern, 3);
-        _dbContext.Texts
-            .Add(new() {
-                Id = 1234,
-                Key = key,
-                ApplicationId = _application.Id,
-                Culture = "en-US",
-                Value = "0.000$",
-            });
-        _dbContext.SaveChanges();
-        var manager = CreateManager();
+        SeedText(key, "0.000$");
 
         // Act
-        var result = manager.FindText(key);
+        var result = _provider.FindText(key);
 
         // Assert
         var subject = result.Should().BeOfType<LocalizedText>().Subject;
@@ -125,11 +118,8 @@ public class PostgreSqlLocalizationProviderTests {
 
     [Fact]
     public void FindList_ReturnsNull_WhenListNotFound() {
-        // Arrange
-        var manager = CreateManager();
-
         // Act
-        var result = manager.FindList("SomeList");
+        var result = _provider.FindList("SomeList");
 
         // Assert
         result.Should().BeNull();
@@ -138,38 +128,10 @@ public class PostgreSqlLocalizationProviderTests {
     [Fact]
     public void FindList_ReturnsListItems_WhenListExists() {
         // Arrange
-        _dbContext.Lists
-                  .Add(new() {
-                      Id = 1234,
-                      Key = "ListKey",
-                      ApplicationId = _application.Id,
-                      Culture = "en-US",
-                      Items = new[] {
-                           new ListItem {
-                               Index = 0,
-                               Text = new() {
-                                   ApplicationId = _application.Id,
-                                   Culture = "en-US",
-                                   Key = "Item1",
-                                   Value = "Item 1"
-                               },
-                           },
-                           new ListItem {
-                               Index = 1,
-                               Text = new() {
-                                   ApplicationId = _application.Id,
-                                   Culture = "en-US",
-                                   Key = "Item2",
-                                   Value = "Item 2"
-                               },
-                           },
-                       },
-                  });
-        _dbContext.SaveChanges();
-        var manager = CreateManager();
+        SeedList("List_key", "label_key", "Some list", 2);
 
         // Act
-        var result = manager.FindList("ListKey");
+        var result = _provider.FindList("List_key");
 
         // Assert
         var subject = result.Should().BeOfType<LocalizedList>().Subject;
@@ -178,11 +140,8 @@ public class PostgreSqlLocalizationProviderTests {
 
     [Fact]
     public void FindImage_ReturnsNull_WhenImageNotFound() {
-        // Arrange
-        var manager = CreateManager();
-
         // Act
-        var result = manager.FindImage("SomeImage");
+        var result = _provider.FindImage("SomeImage");
 
         // Assert
         result.Should().BeNull();
@@ -191,22 +150,176 @@ public class PostgreSqlLocalizationProviderTests {
     [Fact]
     public void FindImage_ReturnsImageByteArray_WhenImageExists() {
         // Arrange
-        _dbContext.Images
-            .Add(new() {
-                Id = 1234,
-                Key = "ImageKey",
-                ApplicationId = _application.Id,
-                Culture = "en-US",
-                Bytes = new byte[] { 1, 2, 3, 4 },
-            });
-        _dbContext.SaveChanges();
-        var manager = CreateManager();
+        SeedImage("image_key", "label_key", "Some image", new byte[] { 1, 2, 3, 4 });
 
         // Act
-        var result = manager.FindImage("ImageKey");
+        var result = _provider.FindImage("image_key");
 
         // Assert
         var subject = result.Should().BeOfType<LocalizedImage>().Subject;
         subject.Bytes.Should().BeEquivalentTo(new byte[] { 1, 2, 3, 4 });
     }
+
+    [Fact]
+    public void SetText_AddsLocalizedText() {
+        var input = new LocalizedText("text_key", "Some text.");
+        // Act
+        _provider.SetText(input);
+
+        // Assert
+        _provider.FindText("text_key").Should().NotBeNull();
+    }
+
+    [Fact]
+    public void SetText_WhenTextExists_UpdatesTextValue() {
+        // Arrange
+        SeedText("text_key", "Old value");
+        var input = new LocalizedText("text_key", "New value");
+
+        // Act
+        _provider.SetText(input);
+
+        // Assert
+        var result = _provider.FindText("text_key");
+        result.Should().NotBeNull();
+        result!.Value.Should().Be("New value");
+    }
+
+    [Fact]
+    public void SetList_WhenListExists_UpdatesListValue() {
+        // Arrange
+        SeedList("list_key", "label_key", "Old label", 2);
+        var input = new LocalizedList("list_key",
+                                      new("label_key", "New label"),
+                                      new[] { new LocalizedText("item1_key", "Item 1") });
+
+        // Act
+        _provider.SetList(input);
+
+        // Assert
+        var result = _provider.FindList("text_key");
+        result.Should().NotBeNull();
+        result!.Label!.Value.Should().Be("New label");
+        result.Items.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void SetImage_WhenImageExists_UpdatesImageValue() {
+        // Arrange
+        SeedImage("image_key", "label_key", "Old label", new byte[] { 1, 2, 3, 4 });
+        var input = new LocalizedImage("image_key", new("label_key", "New label"), new byte[] { 5, 6, 7, 8 });
+
+        // Act
+        _provider.SetImage(input);
+
+        // Assert
+        var result = _provider.FindImage("image_key");
+        result.Should().NotBeNull();
+        result!.Label!.Value.Should().Be("New label");
+        result.Bytes.Should().BeEquivalentTo(new byte[] { 5, 6, 7, 8 });
+    }
+
+    [Fact]
+    public void SetList_AddsLocalizedList() {
+        var input = new LocalizedList("list_key",
+                                      new LocalizedText("label_key", "List Label"),
+                                      new[] {
+                                          new LocalizedText("item1_key", "Item 1"),
+                                          new LocalizedText("item2_key", "Item 2"),
+                                      });
+        // Act
+        _provider.SetList(input);
+
+        // Assert
+        _provider.FindList("list_key").Should().NotBeNull();
+    }
+
+    [Fact]
+    public void SetList_WhenSetExists_UpdateText() {
+        SeedText("list_key", "List Label");
+        var input = new LocalizedList("list_key",
+                                      new LocalizedText("label_key", "List Label"),
+                                      new[] {
+                                          new LocalizedText("item1_key", "Item 1"),
+                                          new LocalizedText("item2_key", "Item 2"),
+                                      });
+        // Act
+        _provider.SetList(input);
+
+        // Assert
+        _provider.FindList("list_key").Should().NotBeNull();
+    }
+
+    [Fact]
+    public void SetImage_AddsLocalizedImage() {
+        var input = new LocalizedImage("image_key",
+                                      new LocalizedText("label_key", "List Label"),
+                                      new byte[] { 1, 2, 3, 4 });
+        // Act
+        _provider.SetImage(input);
+
+        // Assert
+        _provider.FindImage("image_key").Should().NotBeNull();
+    }
+
+    private void SeedText(string key, string value) {
+        _dbContext.Texts
+                  .Add(new() {
+                       Id = 1234,
+                       Key = key,
+                       ApplicationId = _application.Id,
+                       Culture = "en-US",
+                       Value = value,
+                   });
+        _dbContext.SaveChanges();
+    }
+
+    private void SeedList(string key, string labelKey, string labelValue, int itemCount) {
+        _dbContext.Lists
+                  .Add(new() {
+                       Id = 1234,
+                       Key = key,
+                       ApplicationId = _application.Id,
+                       Culture = "en-US",
+                       Label = new Text {
+                           Id = 1234,
+                           Key = labelKey,
+                           ApplicationId = _application.Id,
+                           Culture = "en-US",
+                           Value = labelValue,
+                       },
+                       Items = Enumerable
+                              .Range(0, itemCount)
+                              .Select(i => new ListItem {
+                                   Index = i,
+                                   Text = new() {
+                                       ApplicationId = _application.Id,
+                                       Culture = "en-US",
+                                       Key = $"item{i + 1}_key",
+                                       Value = $"Item {i + 1}"
+                                   },
+                               }).ToArray(),
+                   });
+        _dbContext.SaveChanges();
+    }
+
+    private void SeedImage(string key, string labelKey, string labelValue, byte[] bytes) {
+        _dbContext.Images
+                  .Add(new() {
+                       Id = 1234,
+                       Key = key,
+                       ApplicationId = _application.Id,
+                       Culture = "en-US",
+                       Label = new Text {
+                           Id = 1234,
+                           Key = labelKey,
+                           ApplicationId = _application.Id,
+                           Culture = "en-US",
+                           Value = labelValue,
+                       },
+                       Bytes = bytes,
+                   });
+        _dbContext.SaveChanges();
+    }
+
 }
